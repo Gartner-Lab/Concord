0aeb1f7eb5ac72cc632fd1e474c534615bf6ee96
Qin Zhu <zhuqin@gpudev1.wynton.ucsf.edu>
2024-09-21 19:00:38 -0700

    0921_package1

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Concord',
+    version='0.8.0',
+    description='CONCORD: Contrastive Learning for Cross-domain Reconciliation and Discovery',
+    packages=find_packages(include=['Concord','Concord.*']),
+    author='Qin Zhu',
+    author_email='qin.zhu@ucsf.edu',
+    url='https://github.com/qinzhu/Concord',
+    install_requires=[
+        # dependencies here
+    ],
+    python_requires='>=3.9',
+)
